e95c92bd317cb768ef94ecfe6c1beebb6acd0eec
Matthew <matthew.d.baker20@gmail.com>
2021-06-05 10:49:19 -0500

    rendered temp meal data on page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import './App.css';
 
 import HeadComponent from "./components/Layout/HeadComponent";
 import React from "react";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
       <React.Fragment>
         <HeadComponent />
+          <main>
+              <Meals />
+          </main>
       </React.Fragment>
   );
 }
